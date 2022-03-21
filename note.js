01e050c604f0c116235d8dfb0e21cc226ed4aa0a
Daniel Souza Scremim <75086868+danielscremim@users.noreply.github.com>
2022-03-21 07:45:43 -0300

    Add files via upload

---
+++ note.js
@@ -0,0 +1,34 @@
+/*
+
+<script>
+// variaveis: let ou const
+// váriavel const não pode ser alterado
+// let pode alterar a variável
+let accept = true
+const user = {
+  name: "Daniel S Scremim",
+  phone: 41988984630,
+}
+// chamando a função com o nome da constante e . o objeto Ex:
+
+alert(user.name)
+</script>
+
+//showMeSomething(links.github)
+
+      // variaveis: let ou const
+      //váriavel const não pode ser alterado
+       //let pode alterar a variável
+      //let accept = true
+      //const user = {
+        //name: "Daniel S Scremim",
+        //phone: 41988984630,
+      //}
+      //chamando a função com o nome da constante e . o objeto Ex:
+      
+      //alert(user.name)""""
+
+      camelCase
+      PascalCase
+      snake_case
+*/
